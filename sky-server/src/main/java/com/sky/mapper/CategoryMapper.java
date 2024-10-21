c564fcacd3d35b6fd4a090fc166e2aac88ade761
ryxFry <19818515136@163.com>
2024-10-21 23:07:49 +0800

    分类分页查询

---
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -0,0 +1,17 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CategoryMapper {
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    Page<Category> pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+}
